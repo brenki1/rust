b08164d7331568b22810181d0497bedb0270c3e1
Osvaldo <pagioliosvaldo@gmail.com>
2025-01-15 16:19:50 -0300

    cargo new

---
+++ teste/target/debug/deps/teste-15cd4ea7cd9b8a2b.d
@@ -0,0 +1,5 @@
+/home/pagioli/Documentos/rust/teste/target/debug/deps/libteste-15cd4ea7cd9b8a2b.rmeta: src/main.rs
+
+/home/pagioli/Documentos/rust/teste/target/debug/deps/teste-15cd4ea7cd9b8a2b.d: src/main.rs
+
+src/main.rs:
